54c7a235bf639b634f99b376dc20c54589225255
rhettscronfinkle <goldenfibo1123@gmail.com>
2021-07-24 00:10:53 +0530

    387.Signing Users Up

--- REST_API/1/controllers/auth.js
+++ REST_API/1/controllers/auth.js
@@ -1,5 +1,6 @@
 const { validationResult } = require('express-validator')
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 
 exports.signup = (req, res, next) => {
     
@@ -16,7 +17,26 @@ exports.signup = (req, res, next) => {
     const email = req.body.email;
     const name =  req.body.name;
     const password = req.body.password;
+    bcrypt.hash(password, 12)
+    .then(hashedPw => {
+        const user = new User({
+            email: email,
+            password: hashedPw,
+            name: name
+        });
 
+        return user.save()
+    })
+    .then(result => {
+        res.status(201).json({message: 'User Created', userId: result._id});
+    })
+    .catch(err => {
+        if(!err.statusCode)
+        {
+            err.statusCode = 500;
+        }
+        next(err);
+    })
 
 };
 
